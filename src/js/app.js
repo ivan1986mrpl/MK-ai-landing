646fb6eb4ff6288e1e64c82c779f9eeb5dd38e5a
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-10-26 10:18:38 +0200

    feat: finish news page

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import Header from './modules/Header.js';
 import TabsCollection from './modules/Tabs.js';
+import VideoPlayerCollection from './modules/VideoPlayer.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   new Header();
   new TabsCollection();
+  new VideoPlayerCollection();
 });
