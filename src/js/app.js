d111fcd9a56b2077e5c9bac672e02998251aa402
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-10-31 11:43:33 +0200

    feat: add contacts page

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,16 @@ import ExpandableContentCollection from './modules/ExpandableContent.js';
 import Header from './modules/Header.js';
 import TabsCollection from './modules/Tabs.js';
 import VideoPlayerCollection from './modules/VideoPlayer.js';
+import InputMaskCollection from './modules/InputMask.js';
+import SelectCollection from './modules/Select.js';
+import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   new Header();
   new TabsCollection();
   new VideoPlayerCollection();
   new ExpandableContentCollection();
+  new InputMaskCollection();
+  new SelectCollection();
+  defineScrollBarWidthCSSVar();
 });
